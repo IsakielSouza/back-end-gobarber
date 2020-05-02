8918f1b821ffaa58013cb64f9f1c1a949966c8af
Isakiel Souza <isakielsouza@outlook.com.br>
2020-05-01 23:54:57 -0300

    Autenticaçao 3/4

---
+++ src/services/AuthenticateuserService.ts
@@ -0,0 +1,45 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/Users';
+
+interface Request {
+    email: string;
+    password: string;
+}
+
+interface Response {
+    user: User;
+    token: string;
+}
+
+class AuthenticateUserService {
+    public async execute({ email, password }: Request): Promise<Response> {
+        const usersRepository = getRepository(User);
+
+        const user = await usersRepository.findOne({ where: { email } });
+
+        if (!user) {
+            throw new Error('Incorrect email/password combination.');
+        }
+
+        const passwordMatched = await compare(password, user.password);
+
+        if (!passwordMatched) {
+            throw new Error('Incorrect email/password combination.');
+        }
+
+        const token = sign({}, '0fed20f34895e7dc25bf6a82d5453a80',{
+            subject: user.id,
+            expiresIn: '1d',
+        });
+
+        return {
+            user,
+            token
+        };
+    }
+}
+
+export default AuthenticateUserService;
